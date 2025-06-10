e5b13036d42f9569bc27380e195fb3053271d505
Fisik343 <a.petro343@gmail.com>
2025-06-09 23:13:34 -0400

    Set up mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import "./App.css";
-
 function App() {
   return <div>Hello, if you're seeing this, the basics are working!</div>;
 }
---
+++ src/theme.ts
@@ -0,0 +1,57 @@
+import { createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+
+declare module "@mui/material/styles" {
+  interface BreakpointOverrides {
+    xxl: true;
+  }
+}
+
+// A custom theme for this app
+const initialTheme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 576,
+      md: 768,
+      lg: 992,
+      xl: 1200,
+      xxl: 1450,
+    },
+  },
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#556cd6",
+    },
+    secondary: {
+      main: "#c8a5ff",
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+});
+
+const theme = createTheme(initialTheme, {
+  palette: {
+    white: initialTheme.palette.augmentColor({
+      color: {
+        main: "#ffffff",
+      },
+    }),
+  },
+  typography: {
+    body1: {
+      fontSize: "1.125rem",
+    },
+    body2: {
+      fontSize: "1rem",
+    },
+    h5: {
+      fontSize: "28px",
+    },
+  },
+});
+
+export default theme;
